ae254167bdc4078d717ec3d7bb9d173728dc8f2e
Zain <zmathews@student.wethinkcode.co.za>
2020-11-02 21:22:38 +0200

    fidgeting

--- router/src/main/java/com/zmathews/router/Router.java
+++ router/src/main/java/com/zmathews/router/Router.java
@@ -4,57 +4,55 @@ import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.*;
+// import java.util.concurrent.Future;
+// import java.util.concurrent.TimeUnit;
 
 public class Router {
+	AsynchronousServerSocketChannel marketServer;
+	AsynchronousServerSocketChannel brokerServer;
+
 	public Router() {
 		try {
-			InetAddress host = InetAddress.getByName("localhost");
-			Selector selector = Selector.open();
-			ServerSocketChannel serverSocketChannel =
-				ServerSocketChannel.open();
-			serverSocketChannel.configureBlocking(false);
-			serverSocketChannel.bind(new InetSocketAddress(host, 5000));
-			serverSocketChannel.register(selector, SelectionKey.
-				OP_ACCEPT);
-			SelectionKey key = null;
-			while (true) {
-				if (selector.select() <= 0)
-					continue;
-				Set<SelectionKey> selectedKeys = selector.selectedKeys();
-				Iterator<SelectionKey> iterator = selectedKeys.iterator();
-				while (iterator.hasNext()) {
-					key = (SelectionKey) iterator.next();
-					iterator.remove();
-					if (key.isAcceptable()) {
-						SocketChannel sc = serverSocketChannel.accept();
-						sc.configureBlocking(false);
-						sc.register(selector, SelectionKey.
-							OP_READ);
-						System.out.println("Connection Accepted: "
-							+ sc.getLocalAddress() + "\n");
-					}
-					if (key.isReadable()) {
-						SocketChannel sc = (SocketChannel) key.channel();
-						ByteBuffer bb = ByteBuffer.allocate(1024);
-						sc.read(bb);
-						String result = new String(bb.array()).trim();
-						System.out.println("Message received: "
-							+ result
-							+ " Message length= " + result.length());
-						if (result.length() <= 0) {
-							sc.close();
-							System.out.println("Connection closed...");
-							System.out.println(
-								"Server will keep running. " +
-								"Try running another client to " +
-								"re-establish connection");
-						}
-					}
-				}
-			}
+			marketServer = AsynchronousServerSocketChannel.open();
+			InetSocketAddress mAddress = new InetSocketAddress("localhost", 5001);
+			marketServer.bind(mAddress);
+			System.out.printf("Listening for market connections at: %s%n", mAddress);
+
+			this.initMarket();
+
+			brokerServer = AsynchronousServerSocketChannel.open();
+			InetSocketAddress bAddress = new InetSocketAddress("localhost", 5000);
+			brokerServer.bind(bAddress);
+			System.out.printf("Listening for broker connections at: %s%n", bAddress);
+
+			this.initBroker();
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	private void initBroker() {
+		try {
+			
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private void initMarket() {
+
+	}
+
+	private class DataInstance {
+		int id;
+		AsynchronousServerSocketChannel server;
+		AsynchronousSocketChannel client;
+		ByteBuffer buffer;
+		SocketAddress clientAddr;
+		boolean isRead;
+		// BrokerReadWriteHandler brokerRwHandler;
+		// MarketReadWriteHandler marketRwHandler;
 	}
 }
