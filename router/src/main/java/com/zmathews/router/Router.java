aa4b01b20cae7166b6cc86a5924e7c30865863aa
Zain <zmathews@student.wethinkcode.co.za>
2020-11-05 15:33:08 +0200

    somebody remind me where i am, miami or timbuk2

--- router/src/main/java/com/zmathews/router/Router.java
+++ router/src/main/java/com/zmathews/router/Router.java
@@ -1,20 +1,22 @@
 package com.zmathews.router;
 
+import java.io.IOException;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.Charset;
 import java.util.*;
-// import java.util.concurrent.Future;
-// import java.util.concurrent.TimeUnit;
-
-import javax.xml.crypto.Data;
 
 public class Router {
 	private static int id_;
-	AsynchronousServerSocketChannel marketServer;
-	AsynchronousServerSocketChannel brokerServer;
+	private AsynchronousServerSocketChannel marketServer;
+	private AsynchronousServerSocketChannel brokerServer;
+	private Map<Integer, DataInstance> markets;
+	private Map<Integer, DataInstance> brokers;
 
 	public Router() {
+		markets = new HashMap<Integer, DataInstance>();
+		brokers = new HashMap<Integer, DataInstance>();
 		try {
 			id_ = 100000;
 			marketServer = AsynchronousServerSocketChannel.open();
@@ -49,8 +51,70 @@ public class Router {
 						newInstance.id = Router.incId();
 						newInstance.server = dataInstance.server;
 						newInstance.client = ch;
+						newInstance.buffer = ByteBuffer.allocate(2048);
+						newInstance.isRead = true;
 						newInstance.cliAddress = cliAddress;
+						Charset cs = Charset.forName("UTF-8");
 						String mString = "ID: " + Integer.toString(newInstance.id);
+						newInstance.buffer.clear();
+						byte[] bytes = mString.getBytes(cs);
+						newInstance.buffer.put(bytes);
+						newInstance.buffer.flip();
+						brokers.put(newInstance.id, newInstance);
+						newInstance.client.write(newInstance.buffer);
+						Router.clearBuffer(newInstance);
+						newInstance.client.read(newInstance.buffer, newInstance, new CompletionHandler<Integer, DataInstance>() {
+							public void completed(Integer result, DataInstance dataInstance) {
+								if (result == -1) {
+									try {
+										dataInstance.client.close();
+										System.out.format("\u001b[31mStopped listening to the Broker %s ID %s%n\u001b[0m", dataInstance.cliAddress, dataInstance.id);
+									}
+									catch (IOException ex) {
+										ex.printStackTrace();
+									}
+									return ;
+								}
+								if (dataInstance.isRead) {
+									dataInstance.buffer.flip();
+									int limits = dataInstance.buffer.limit();
+									byte bytes[] = new byte[limits];
+									dataInstance.buffer.get(bytes, 0, limits);
+									Charset cs = Charset.forName("UTF-8");
+									String msg = new String(bytes, cs);
+									System.out.format("\u001b[33mBroker(%s) ID(%s): %s%n\u001b[0m", dataInstance.cliAddress, dataInstance.id, msg);
+									String[] parts = msg.split("\\|");
+									if (parts.length == 7) {
+										int marketID = Integer.parseInt(parts[0]);
+										int price = Integer.parseInt(parts[3]);
+										int checksum = Integer.parseInt(parts[6]);
+										int msglen = parts[0].length() + parts[1].length() + parts[2].length() + parts[3].length() + parts[4].length() + parts[5].length() + 5;
+										if (checksum - price == msglen) {
+											if (writeToMarket(msg, marketID) == 0) {
+												String newMsg = "Market: " + parts[0] + " Does not exist";
+												writeToBroker(newMsg, Integer.parseInt(parts[5]));
+												return ;
+											}
+										}
+										dataInstance.buffer.clear();
+										byte[] data = msg.getBytes(cs);
+										dataInstance.buffer.put(data);
+										dataInstance.buffer.flip();
+										dataInstance.isRead = false; // It is a write
+										Router.clearBuffer(dataInstance);
+									}
+								}
+								else {
+									dataInstance.isRead = true;
+									dataInstance.buffer.clear();
+									dataInstance.client.read(dataInstance.buffer, dataInstance, this);
+								}
+							}
+
+							public void failed(Throwable e, DataInstance dataInstance) {
+								e.printStackTrace();
+							}
+						});
 					}
 					catch (Exception e) {
 						System.out.println(e.getMessage());
@@ -68,7 +132,7 @@ public class Router {
 	}
 
 	private void initMarket() {
-
+		//extreme laze
 	}
 
 	private class DataInstance {
@@ -78,12 +142,119 @@ public class Router {
 		ByteBuffer buffer;
 		SocketAddress cliAddress;
 		boolean isRead;
-		// BrokerReadWriteHandler brokerRwHandler;
-		// MarketReadWriteHandler marketRwHandler;
 	}
 
 	public static int incId() {
 		id_++;
 		return (id_);
+	}
+
+	public static void clearBuffer(DataInstance dataInstance) {
+		String msg = "";
+		dataInstance.buffer.clear();
+		Charset cs = Charset.forName("UTF-8");
+		byte[] data = msg.getBytes(cs);
+		dataInstance.buffer.put(data);
+	}
+
+	public int writeToMarket(String msg, int marketID) {
+		try {
+			DataInstance market = markets.get(marketID);
+			Charset cs = Charset.forName("UTF-8");
+			byte[] data = msg.getBytes(cs);
+			market.buffer.clear();
+			market.buffer.put(data);
+			market.buffer.flip();
+			market.client.write(market.buffer);
+			clearBuffer(market);
+			market.isRead = true;
+			try {
+				market.client.read(market.buffer, market, new CompletionHandler<Integer,DataInstance>(){
+					//am laze
+				});
+			}
+			catch(ReadPendingException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		catch(NullPointerException e) {
+			return(0);
+		}
+		return(1);
+	}
+
+	public int writeToBroker(String msg, int brokerID) {
+		try {
+			DataInstance broker = brokers.get(brokerID);
+			Charset cs = Charset.forName("UTF-8");
+			byte[] data = msg.getBytes(cs);
+			broker.buffer.clear();
+			broker.buffer.put(data);
+			broker.buffer.flip();
+			broker.client.write(broker.buffer);
+			clearBuffer(broker);
+			broker.isRead = true;
+			try {
+				broker.client.read(broker.buffer, broker, new CompletionHandler<Integer, DataInstance>(){
+					public void completed(Integer result, DataInstance dataInstance) {
+						if (result == -1) {
+							try {
+								dataInstance.client.close();
+								System.out.format("\u001b[31mStopped listening to the Broker %s ID %s%n\u001b[0m", dataInstance.cliAddress, dataInstance.id);
+							}
+							catch (IOException ex) {
+								ex.printStackTrace();
+							}
+							return ;
+						}
+						if (dataInstance.isRead) {
+							dataInstance.buffer.flip();
+							int limits = dataInstance.buffer.limit();
+							byte bytes[] = new byte[limits];
+							dataInstance.buffer.get(bytes, 0, limits);
+							Charset cs = Charset.forName("UTF-8");
+							String msg = new String(bytes, cs);
+							System.out.format("\u001b[33mBroker(%s) ID(%s): %s%n\u001b[0m", dataInstance.cliAddress, dataInstance.id, msg);
+							String[] parts = msg.split("\\|");
+							if (parts.length == 7) {
+								int marketID = Integer.parseInt(parts[0]);
+								int price = Integer.parseInt(parts[3]);
+								int checksum = Integer.parseInt(parts[6]);
+								int msglen = parts[0].length() + parts[1].length() + parts[2].length() + parts[3].length() + parts[4].length() + parts[5].length() + 5;
+								if (checksum - price == msglen) {
+									if (writeToMarket(msg, marketID) == 0) {
+										String newMsg = "Market: " + parts[0] + " Does not exist";
+										writeToBroker(newMsg, Integer.parseInt(parts[5]));
+										return ;
+									}
+								}
+								dataInstance.buffer.clear();
+								byte[] data = msg.getBytes(cs);
+								dataInstance.buffer.put(data);
+								dataInstance.buffer.flip();
+								dataInstance.isRead = false; // It is a write
+								Router.clearBuffer(dataInstance);
+							}
+						}
+						else {
+							dataInstance.isRead = true;
+							dataInstance.buffer.clear();
+							dataInstance.client.read(dataInstance.buffer, dataInstance, this);
+						}
+					}
+
+					public void failed(Throwable e, DataInstance dataInstance) {
+						e.printStackTrace();
+					}
+				});
+			}
+			catch(ReadPendingException e) {
+				System.out.println("readPending");
+			}
+		}
+		catch(NullPointerException e) {
+			return(0);
+		}
+		return(1);
 	}
 }
